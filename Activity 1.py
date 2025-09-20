5f97f33b9b9ef0edf3cedfe7ea0718325771c929
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-09-20 11:10:59 +0530

    A1

---
+++ Activity 1.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("Welcome to the world of programming!")
+#print a number
+print(4)
+#Next line after print
+print("hello python \n")
+#Pass multiple arguments for printing
+print("hello,5")
+#End argument of Print statement
+print("welcome to", end= "*")
