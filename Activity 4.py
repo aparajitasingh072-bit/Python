6bdd9871a2f4960178d6e75bebe3298b3ff115b0
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-09-22 18:04:36 +0530

    A4

---
+++ Activity 4.py
@@ -0,0 +1,13 @@
+print(" Birthdays of 5 friends\n")
+
+#storing Birthdays in variables and printing
+a="13/02/92"
+print("Aakash -",a)
+b="16/05/952"
+print("\nsunny -",b)
+c="06/05/93"
+print("\nsweety-",c)
+d="12/02/92"
+print("\npooja-", d)
+e="05/09/96"
+print("\nram-", e)
