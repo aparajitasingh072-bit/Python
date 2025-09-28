d3d2f22ec3e9bd09e5d351e6a31adc8a11068995
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-09-28 16:29:14 +0530

    Count the notes

---
+++ Count the notes.py
@@ -0,0 +1,12 @@
+#Taking total amount as input from the user
+Amount=int(input('Please enter Amount for Widhdraw:'))
+
+#calculating the number of notes of different denominations
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+print("notes of 100 rupee", note_1)
+print("notes of 50 rupee", note_2)
+print("notes of 10 rupee", note_3)
+
