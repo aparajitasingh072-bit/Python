d1d61771592af202e96bcc76389fcc3518892680
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-09-29 19:28:59 +0530

    conditional statements

--- Percentage calculation.py
+++ Percentage calculation.py
@@ -1,15 +1,17 @@
 #Take marks input from user
-print("enter marks obtained in 4 subjects:")
+print("enter marks obtained in 6 subjects:")
 Maths = int(input("maths:"))
 English = int(input("English:"))
 Science = int(input("Science:"))
 Hindi = int(input("Hindi:"))
+Socialsci = int(input("Socialsci:"))
+ger = int(input("ger:"))
 
 #Lets calculate the percentage of marks
 sum=Maths+English+Science+Hindi
-print('sum of math, english, science and hindi:')
+print('sum of math, english, science, hindi, Socialsci and ger:')
 
-perc = (sum/400)*100
+perc = (sum/480)*100
 
 print(end="Percentage Mark=")
 print(perc)
