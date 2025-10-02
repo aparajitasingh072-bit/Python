ef237671182f600a6f4fd871a63ac24fe6bde3a3
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-10-02 12:24:19 +0530

    Checking Alphabets

---
+++ Checking Alphabets.py
@@ -0,0 +1,10 @@
+print("enter a character")
+c=input( )
+if c>='0' and c<='9':
+    print("\nIt is a Number")
+elif c>='a' and c<='z':
+    print("\nIt is an alphabet")
+elif c>='A' and c<='Z':
+    print("\nIt is an alphabet")
+else: 
+    print("\nIt is not an alphabet nor a number!")
