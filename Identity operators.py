46e95d24241bb86e7c22575b74028faf5f609184
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-10-11 12:28:27 +0530

    identity operators

---
+++ Identity operators.py
@@ -0,0 +1,19 @@
+#python program to illustrate the use of "is" identity operator
+x = 5
+if (type(x)is int):
+    print("true")
+else:
+    print("false")
+
+x= 5.0
+if (type(x)is not float):
+    print("true")
+else:
+    print("false")
+
+x = 20
+y = 20
+if (x is y):
+    print("x and y have SAME identity")
+else:
+    ("x and y have DIFFERENT identity")
