5dcf78c0013574a26bed9d521728581be236ad5d
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-10-13 19:28:00 +0530

    operators presedence

---
+++ Ascii.py
@@ -0,0 +1,15 @@
+x = input("Enter the ASCII code between 65-90: ")
+
+if x == '65':
+    print=('the ASCII code for 65 stands for alphabet A')
+elif x=='66':
+    print("the ASCII code for 66 stands for alphabet B")
+elif x=='67':
+    print("the ASCII code for 66 stands for alphabet C")
+elif x=='68':
+    print("the ASCII code for 66 stands for alphabet D")
+elif x=='69':
+    print("the ASCII code for 66 stands for alphabet E")
+
+
+
