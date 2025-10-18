c02648eeb72eea3e3cf0340c66c196b8206698f8
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-10-18 10:57:54 +0530

    Exam eligibility

---
+++ Exam eligibility.py
@@ -0,0 +1,14 @@
+#take input for the student that he can attend the exam or not
+medical_cause=input('did you have any medical cause Y or N: ')
+#take input of the attendence
+atten= int(input('enter the attendence of the student: '))
+
+#checking the user input predicting output accordingly
+
+if medical_cause=='Y': #checking the condition 1
+    print('you are allowed')
+else: 
+    if atten>75: #checking the condition 2
+        print('allowed')
+    else: 
+        print ('not allowed')
