581d9024fe56084819f41c4c85bc8a9722f29e8a
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-10-23 14:55:31 +0530

    sum of natural numbers

---
+++ sum of natural numbers.py
@@ -0,0 +1,10 @@
+#input the value of terms
+n = int(input("enter the value of terms: "))
+
+sum = 0
+i = 1
+while i<=n: #loop will run from 1 to n
+    sum = sum + i
+    i = i + 1
+
+print ("\nSum= ", sum)
