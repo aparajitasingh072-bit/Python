c46bea510fb213fa34ce42801a5ae44b296a1311
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2025-12-08 19:02:43 +0530

    thats the exit.py

---
+++ thats the exit.py
@@ -0,0 +1,14 @@
+#python program to demonstrate
+#exit()
+
+for i in range(10):
+    #If the value of i becomes 
+    #5 then the program is forced
+    #to exit
+    if i == 5:
+
+        #Prints the exit message
+        print(exit)
+        exit()
+
+    print(i)
