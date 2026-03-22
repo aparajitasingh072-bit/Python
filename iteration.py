3b9bcd8877e39863ebf1d9da2a9991399359bbc8
aparajitasingh072-bit <aparajitasingh072@gmail.com>
2026-03-22 12:01:13 +0530

    iterations

--- iteration.py
+++ iteration.py
@@ -5,3 +5,33 @@ def fun1(n):
 print(fun1(4))
 
 print("So, the number of iterations will be 1 for any input")
+
+def fun2(n):
+
+    sum=0
+
+    for i in range(1, n+1):
+
+        sum+=i
+
+    return sum
+
+print(fun2(4))
+
+print("So, the number of iterations will be 1+1+1+1 = 4 = n(input)iterations")
+
+def fun3(n):
+
+    sum = 0
+    
+    for i in range(1,n+1):
+        
+        for j in range(1, i+1):
+
+            sum+=1
+
+    return sum
+
+print(fun3(4))
+
+print("So, number of iterations will be 1 + 2 + 3 + 4 = 10")
